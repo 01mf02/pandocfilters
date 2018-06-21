c18b56edaed756776792ea394b8cab6ec50085b7
Michael Färber <michael.faerber@gedenkt.at>
2018-06-21 20:44:52 +0200

    New filter to index links.

---
+++ linkindex.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Pandoc filter to convert links with a title to \index calls.
+"""
+
+# debugging
+from __future__ import print_function
+import sys
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+from pandocfilters import toJSONFilter, RawInline, Str, stringify
+
+def latex(x):
+    return RawInline('latex', x)
+
+def linkindex(key, value, format, meta):
+    #eprint("Key: " + key)
+    if key == 'Link':
+        #eprint(value)
+        name = value[1]
+        url = value[2][0]
+        title = value[2][1]
+
+        if url == '#' and len(title) == 0:
+            title = stringify(name)
+
+        if len(title) > 0:
+            if format == "latex":
+                return name + [latex('\index{' + title + '}')]
+
+if __name__ == "__main__":
+    toJSONFilter(linkindex)
