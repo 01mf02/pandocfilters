baa17ac0e1f407081347c72d2c0b1c01300ceea0
Michael Färber <michael.faerber@gedenkt.at>
2018-06-27 18:17:50 +0200

    Clean up listing filter.

--- listing.py
+++ listing.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+# Wrap code blocks in listing environment with caption
+
+# By default, Pandoc adds captions to code blocks only
+# when using the `--listings` option.
+# However, the output of the `listings` is
+# not as aesthetically pleasing as the one from Pandoc.
+# I therefore add caption support to Pandoc without `--listings`.
+
 # debugging
 from __future__ import print_function
 import sys
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-from pandocfilters import toJSONFilter, RawInline, Para
-from subprocess import Popen, PIPE
+from pandocfilters import toJSONFilter, RawInline, Plain, CodeBlock
 
 def latex(x):
     return RawInline('latex', x)
@@ -25,27 +32,17 @@ def get_value(kv, key, value = None):
 def listing(key, value, format, meta):
     #eprint("Key: " + key)
     if key == 'CodeBlock':
-        ident = value[0][0]
-        classes = value[0][1]
-        kvs = value[0][2]
-        code = value[1]
-        (caption, _) = get_value(kvs, 'caption')
+        (caption, _) = get_value(value[0][2], 'caption')
         
-        dclasses = " ".join(map ((lambda x: "." + x), classes))
-        stdin = '~~~ {' + dclasses + '}\n' + code + '\n~~~'
+        block = CodeBlock(value[0], value[1])
 
-        p = Popen(['pandoc', '-t', 'latex'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = p.communicate(stdin)
-
-	lines = \
-            [ r'\begin{listing}' \
-            , stdout \
-	    , r'\caption{' + caption + '}'
-	    , r'\label{' + ident + '}' \
-	    , r'\end{listing}'
-	    ]
-	return Para([latex('\n'.join(lines))])
-
+        # we only need to add the caption, label is handled by Pandoc
+        return \
+            [ Plain([latex(r'\begin{listing}' + '\n' + \
+                           r'\caption{' + caption + '}')]) \
+            , block \
+            , Plain([latex(r'\end{listing}')]) \
+            ]
 
 if __name__ == "__main__":
     toJSONFilter(listing)
